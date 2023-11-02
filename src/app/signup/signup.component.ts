c572078ba27a3ef4b992f3afe43a9b6aa03b08c4
divietroriccardo <riki.div.15@gmail.com>
2023-11-02 14:04:03 +0100

    Add snackbar service and implemented `formGroup`

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { FormControl, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 import { UserService } from '../user.service';
 import { PageService } from '../page.service';
+import { SnackbarService } from '../snackbar.service';
 
 @Component({
   selector: 'app-signup',
@@ -13,30 +13,35 @@ import { PageService } from '../page.service';
   styleUrls: ['./signup.component.scss'],
 })
 export class SignupComponent {
-  isPasswordHidden: boolean = true;
   isRepeatedPasswordHidden: boolean = true;
 
   showDetails: boolean = false;
 
-  username = new FormControl('', [
-    Validators.required,
-    Validators.pattern('[0-9A-Za-z_.]{6,16}$'),
-  ]);
-  phoneNumber = new FormControl('', [
-    Validators.required,
-    Validators.pattern('[- +()0-9]+'),
-  ]);
-  email = new FormControl('', [Validators.required, Validators.email]);
-  password = new FormControl('', [
-    Validators.required,
-    Validators.minLength(8),
-  ]);
-  repeatedPassword = new FormControl('', [Validators.required]);
+  signupFields = new FormGroup({
+    username: new FormControl('', [
+      Validators.required,
+      Validators.pattern('[0-9A-Za-z_.]{6,16}$'),
+    ]),
+
+    phoneNumber: new FormControl('', [
+      Validators.required,
+      Validators.pattern('[- +()0-9]+'),
+    ]),
+
+    email: new FormControl('', [Validators.required, Validators.email]),
+
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+    ]),
+
+    repeatedPassword: new FormControl('', [Validators.required]),
+  });
 
   constructor(
     private pageService: PageService,
     private userService: UserService,
-    private snackBar: MatSnackBar,
+    private snackBar: SnackbarService,
     private router: Router
   ) {}
 
@@ -47,60 +52,66 @@ export class SignupComponent {
   }
 
   signup() {
-    if (this.isAllRight()) {
+    if (!this.signupFields.invalid) {
       this.userService
         .signup(
-          this.username.value || '',
-          this.email.value || '',
-          this.phoneNumber.value || '',
-          this.password.value || ''
+          this.signupFields.value.username || '',
+          this.signupFields.value.email || '',
+          this.signupFields.value.phoneNumber || '',
+          this.signupFields.value.password || ''
         )
         .subscribe({
           next: (resp) => {
             if (resp == 'Username already exist') {
-              this.snackBarFunction(
+              this.snackBar.open(
                 'Questo nome utente non è disponibile. Prova con un altro'
               );
             } else if (resp == 'Email already exist') {
-              this.snackBarFunction(
+              this.snackBar.open(
                 'Un altro account usa lo stesso indirizzo email'
               );
             } else if (resp == 'Phone number already exist') {
-              this.snackBarFunction(
+              this.snackBar.open(
                 'Un altro account usa lo stesso numero di telefono'
               );
             } else {
-              this.snackBarFunction('Registrazione effettuata');
+              this.snackBar.open('Registrazione effettuata');
 
               this.router.navigate(['/login']);
             }
           },
           error: (err) => {
-            this.snackBarFunction('Errore. Prova di nuovo');
+            this.snackBar.open('Errore. Prova di nuovo');
             console.log(err);
           },
         });
+    } else {
+      this.signupFields.value.password ==
+        this.signupFields.value.repeatedPassword ||
+      this.signupFields.get('repeatedPassword')?.invalid
+        ? this.snackBar.open('Inserire tutti i campi correttamente')
+        : this.snackBar.open('Le password non corrispondono');
     }
   }
 
   getErrorMessage(field: string): string {
     switch (field) {
       case 'username':
-        if (this.username.hasError('required')) {
+        if (this.signupFields.get('username')?.hasError('required')) {
           return 'Inserisci un username';
         } else {
           return 'Username non valido';
         }
 
       case 'email':
-        if (this.email.hasError('required')) {
+        if (this.signupFields.get('email')?.hasError('required')) {
           return 'Inserisci la tua email';
         } else {
           return 'Email non valida';
         }
 
       case 'phoneNumber':
-        if (this.phoneNumber.hasError('required')) {
+        if (this.signupFields.get('phoneNumber')?.hasError('required')) {
           return 'Inserisci il tuo numero di telefono';
         } else {
           return 'Numero di telefono valido';
@@ -110,7 +121,7 @@ export class SignupComponent {
         return 'Ripeti la password';
 
       case 'password':
-        if (this.password.hasError('required')) {
+        if (this.signupFields.get('password')?.hasError('required')) {
           return 'Inserisci una password';
         } else {
           return 'La password è troppo corta';
@@ -119,35 +130,5 @@ export class SignupComponent {
       default:
         return '';
     }
-  }
-
-  isAllRight() {
-    if (
-      !this.username.hasError('required') &&
-      !this.username.hasError('pattern') &&
-      !this.password.hasError('required') &&
-      !this.password.hasError('minlenght') &&
-      !this.repeatedPassword.hasError('required') &&
-      !this.email.hasError('required') &&
-      !this.email.hasError('email') &&
-      !this.phoneNumber.hasError('required') &&
-      !this.phoneNumber.hasError('pattern') &&
-      !(this.password.value != this.repeatedPassword.value)
-    ) {
-      return true;
-    } else {
-      this.password.value == this.repeatedPassword.value ||
-      this.repeatedPassword.invalid
-        ? this.snackBarFunction('Inserire tutti i campi correttamente')
-        : this.snackBarFunction('Le password non corrispondono');
-
-      return false;
-    }
-  }
-
-  snackBarFunction(text: string): void {
-    this.snackBar.open(text, 'Rimuovi', {
-      duration: 3000,
-    });
   }
 }
